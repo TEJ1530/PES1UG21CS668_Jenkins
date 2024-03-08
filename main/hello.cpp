0f413c63deda95ec555b63d959eec9bd2c80db66
Teja Yadav <67729231+TEJ1530@users.noreply.github.com>
2024-03-08 23:41:37 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  COUT << "webhook has been added";
+  cout << "webhook has been added";
   
   return 0;
 }
